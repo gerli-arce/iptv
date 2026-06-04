743c020addb906721fb121d3486edd0283a1c4ef
unknown <edwininfantearce@gmail.com>
2026-06-04 13:55:49 -0500

    agregado vase de datos

--- resources/js/services/homeApi.js
+++ resources/js/services/homeApi.js
@@ -1,4 +1,4 @@
-const DEFAULT_HOME_API_URL = "http://127.0.0.1:8000/api/home";
+const DEFAULT_HOME_API_URL = "/api/home";
 
 export function getHomeApiUrl() {
   return import.meta.env.VITE_HOME_API_URL || DEFAULT_HOME_API_URL;
